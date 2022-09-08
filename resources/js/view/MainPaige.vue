6bfcf63d3cebfc9e35203d6a3e2d9beaa9d6d16e
zhenya <yauheni.turban@innowise-group.com>
2022-09-08 19:39:47 +0300

    added button to signin

--- resources/js/view/MainPaige.vue
+++ resources/js/view/MainPaige.vue
@@ -1,13 +1,21 @@
 <template>
-    <h1>
-        Hi there!
-    </h1>
+    <div class="flex">
+        <h1>
+            Hi there!
+        </h1>
+        <button type="button" @click="signin">Sign In</button>
+    </div>
 
 </template>
 
 <script>
 export default {
     name: "MainPaige",
+    methods: {
+        signin: function () {
+            this.$router.push({name: "signin"})
+        }
+    }
 }
 </script>
 
