fc45a8ab3dffa141bf3b2240c97e5bf07dde2345
zhenya <yauheni.turban@innowise-group.com>
2022-09-09 20:14:38 +0300

    header with tailwind in the maint page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Laravel</title>
-    <link
-        href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
-        rel="stylesheet"
-    />
-    <link
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;900&display=swap"
-        rel="stylesheet"
-    />
+
     <meta
         name="viewport"
         content="width=device-width,initial-scale=1,maximum-scale=1"
